eb1bb7757cac1d54c85a4c37d8dd9c69e06f7276
Vishal Dilwale <vishal.dilwale@mindbowser.com>
2023-04-09 13:27:13 +0530

    test case add status

--- test_case/test_auth.py
+++ test_case/test_auth.py
@@ -2,6 +2,6 @@ import pytest
 import requests
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
-
+from utilities import testDataGen
 def test_token_gen():
     pass
