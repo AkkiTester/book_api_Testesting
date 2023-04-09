0f910a1c1bd428bc4e18f7fef9c1a7b35cc65741
Vishal Dilwale <vishal.dilwale@mindbowser.com>
2023-04-09 15:18:54 +0530

    test case add status

--- test_case/test_auth.py
+++ test_case/test_auth.py
@@ -9,13 +9,18 @@ data = {
    "clientName": testDataGen.nameLen(),
    "clientEmail": testDataGen.emailgen()
     }
-token=''
+
 def test_token_gen():
     LogGen.loggen().info("************Test_Auth_URL***********")
     response=requests.post(ReadConfig.getAuth(),json=data)
-    token=response.json()
+    global token
+    token = (json.loads(response.text)["accessToken"])
     print(token)
     assert response.status_code==201
     LogGen.loggen().info("************Test_Auth_URL_Passed***********")
 
-print(token)
+def test_token_gen_alredy_used_email():
+    LogGen.loggen().info("************Test_Auth_URL_gen_alredy_used_email***********")
+    response=requests.post(ReadConfig.getAuth(),json=data)
+    assert response.status_code==409
+    LogGen.loggen().info("************Test_Auth_URl_gen_alredy_used_email_passed***********")
