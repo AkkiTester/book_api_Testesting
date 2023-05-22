9e6d39f701bdda981bab631f42fd472c12a0424c
Vishal Dilwale <vishal.dilwale@mindbowser.com>
2023-05-22 19:10:02 +0530

    test case add status

--- test_case/test_base_url.py
+++ test_case/test_base_url.py
@@ -11,9 +11,25 @@ def test_base_url():
     assert response.status_code==200
     LogGen.loggen().info("************Test_Base_URL_Passed***********")
 
+def test_base_url_time():
+    LogGen.loggen().info("************Test_Base_URL_time1sec***********")
+    response=requests.get(ReadConfig.getBaseURL(),timeout=1.5)
+    print(response.text)
+    assert response.status_code==200
+    LogGen.loggen().info("************Test_Base_URL_time1_Passed***********")
+
+
+
 def test_url_status():
     LogGen.loggen().info("************Test_URL_status***********")
     response=requests.get(ReadConfig.getUrlStatus())
+    print(response.text)
+    assert response.status_code==200
+    LogGen.loggen().info("************Test_URL_status_Passed***********")
+
+def test_url_status_time():
+    LogGen.loggen().info("************Test_URL_status_time***********")
+    response=requests.get(ReadConfig.getUrlStatus(),timeout=1)
     print(response.text)
     assert response.status_code==200
     LogGen.loggen().info("************Test_URL_status_Passed***********")
